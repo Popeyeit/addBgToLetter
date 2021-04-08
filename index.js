fcf2593da8f99eb50c88636aad0f62dc0dd9a452
PopeyeIT <IgorSerov28@gmail.com>
2021-04-08 08:04:21 -0400

    1.1v

--- index.js
+++ index.js
@@ -27,92 +27,137 @@ let correctWords = []
 let counter = 0
 
 
-const addBg = (inputText)=>{const  spans = correctWords.map(element => {
-           return [...element.querySelectorAll('span')]
-});
-    spans.forEach(el => {
-          
-            const content = el[inputText.length - 1]
-            
 
-            if (inputText.length > el.length) {
-                return
-            }
-         
-            if (content.textContent === inputText[inputText.length - 1]) {
-                content.style.backgroundColor = 'red';
-            }
-        })
-}
-        
-const deleteBg = (inputText) => {
 
-    const  spans = correctWords.map(element => {
-           return [...element.querySelectorAll('span')]
-           
+const addBg = (inputText) => {
+
+
+    const spans = correctWords.map(element => {
+        return [...element.querySelectorAll('span')]
     });
     
-
-    spans.forEach(el => {
-        let content = ''
+    
+    for (let index = 0; index < inputText.length; index++) {
+        spans.forEach(element => {
+              if (element[index]) {
+                  if (element[index].textContent === inputText[index]) {
+                
+                const text = element.map(el => el.textContent).join('')
+                  const newText = text.slice(0,inputText.length)
         
-        if (inputText.length > 0) {
-         content = el[inputText.length - 1]
-        } else {
-             content = el[0]
+                if (newText === inputText) {
+                  
+                    element[index].style.backgroundColor = 'red';
+           
+                        if (element[index].previousElementSibling) {
+                            if (element[index].previousElementSibling.style.backgroundColor === 'red')
+                            
+                            
+                            {
+                                element[index].style.backgroundColor = 'red';
+                            
+                            }
+                        }
+            
+                        }
+          
+                    }}
+            
+        });
+   
+            }
+        }
+
+
+const deleteBg = (inputText) => {
+
+    const spans = correctWords.map(element => {
+        return [...element.querySelectorAll('span')]
+           
+    });
+
+    for (let index = 0; index < inputText.length + 1; index++) {
+       
+        spans.forEach(element => {
+                if (inputText.length === 0) {
+                element[0].style.backgroundColor = 'inherit'
+            
+                }
+         
+
+            if (element[index]) {
+                if (element[index].textContent === inputText[index]) {
+                    
+                
+                    const text = element.map(el => el.textContent).join('')
+                    const newText = text.slice(0, inputText.length)
+        
+                    if (newText === inputText) {
+                        if (element[inputText.length - 1].nextSibling.style) {
+           
+
+                            element[inputText.length-1].nextSibling.style.backgroundColor = 'inherit';
+                        }
+               
+          
+                    }
+                      
+                }
+            }
+
+
+        }
+            );
+   
     }
-       
-  
-              if (content.textContent === inputText[inputText.length - 1]) {
-            content.nextSibling.style.backgroundColor = 'inherit';
-        }
-     
-        if (inputText.length === 0) {
-            content.style.backgroundColor = 'inherit'
-       
-        
-        }
-        })
+ 
+    console.log(counter);
+    console.log(inputText.length);
 
+    if (counter > inputText.length) {
+        spans.forEach(el => {
+            console.log(el)
+        })
+    }
 }
 const findWord = (e) => {
     const inputText = e.currentTarget.value
 
-if(inputText.length < 2){    for (let index = 0; index < allWordsInDom.length; index++) {
-        const element = allWordsInDom[index];
-    const text = element.querySelector('span').textContent
 
-        if (text === inputText[0] && inputText.length > 0 && correctWords.length === 0) {
-            correctWords = [...correctWords ,allWordsInDom[index] ]
+
+    if (correctWords.length < arrValues.length && correctWords.length=== 0) {
+        for (let index = 0; index < allWordsInDom.length; index++) {
+            const element = allWordsInDom[index];
+            const text = [...element.querySelectorAll('span')].map(el => el.textContent).join('')
+            const newText = text.slice(0, inputText.length)
+            if (newText === inputText && inputText.length > 0) {
+                correctWords = [...correctWords, allWordsInDom[index]]
+            }
         }
-}
+    }
     
-}
-  
 
-    if (counter >= inputText.length) {
+      if (counter < inputText.length) {
+           if (correctWords.length) {
+       addBg(inputText)
+           }
+          if (counter === 0 && inputText.length > 1) {
+              counter = inputText.length
+          }
+        counter++
+    }
+
+    if (counter >= inputText.length || counter===0 && inputText.length > 1) {
         deleteBg(inputText)
         counter = inputText.length
     }
 
-
-    if (counter < inputText.length) {
-           if (correctWords.length) {
-       addBg(inputText)
-    }
-        counter++
-    }
-    
  if (inputText.length === 0) {
         correctWords = []
-    }
+ }
+
 }
 
 
-
+        
 const input = document.querySelector('#search').addEventListener('input',findWord)
-
-
-
-
-
